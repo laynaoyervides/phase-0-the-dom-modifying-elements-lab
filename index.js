901d2131bc61abcf3d874d7871d758c278b83a38
Layna Oyervides <laynaoyervides@gmail.com>
2021-07-26 11:59:51 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+const newHeader = document.createElement('H1'); 
+newHeader.id = "victory";
+newHeader.innerHTML ="YOUR-NAME is the champion";
+document.body.appendChild(newHeader); 
+
